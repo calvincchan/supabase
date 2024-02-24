640e1cf89b12cb866f73c5cda5b562fc4d313754
Calvin <calvin@edu.ink>
2024-02-23 18:43:46 -0800

    chore: backup to pending members

---
+++ docker/supabase/pending_member.sql
@@ -0,0 +1,45 @@
+-- -------------------------------------------------------------
+-- TablePlus 5.9.0(538)
+--
+-- https://tableplus.com/
+--
+-- Database: postgres
+-- Generation Time: 2024-02-23 12:40:56.4690
+-- -------------------------------------------------------------
+INSERT INTO
+  "public"."pending_member" (
+    "id",
+    "name",
+    "role",
+    "invited_at",
+    "activated_at"
+  )
+VALUES
+  (
+    'afung@isf.edu.hk',
+    'Adams',
+    'IT Admin',
+    '2024-01-20 04:31:20.280972+00',
+    NULL
+  ),
+  (
+    'performer05@isf.edu.hk',
+    'EDU.INK Tester',
+    'IT Admin',
+    '2024-01-20 04:21:45.838769+00',
+    NULL
+  ),
+  (
+    'tpcheng@isf.edu.hk',
+    'Tin Cheng',
+    'IT Admin',
+    '2024-01-20 04:31:20.280972+00',
+    NULL
+  ),
+  (
+    'ylfu@isf.edu.hk',
+    'Annie Fu',
+    'IT Admin',
+    '2024-01-20 04:31:20.280972+00',
+    NULL
+  );
