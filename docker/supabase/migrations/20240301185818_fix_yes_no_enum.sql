5f0763ffc179fd781c2718fdb9cc4ff5a6d8e6ec
Calvin <calvin@edu.ink>
2024-03-01 11:20:06 -0800

    fix: type casting on migrating enum

--- docker/supabase/migrations/20240301185818_fix_yes_no_enum.sql
+++ docker/supabase/migrations/20240301185818_fix_yes_no_enum.sql
@@ -6,30 +6,27 @@ ALTER TABLE "public"."case"
 ALTER COLUMN "pa_report"
 DROP DEFAULT;
 
-ALTER TYPE "public"."yes_no_enum"
-RENAME TO "yes_no_enum__old_version_to_be_dropped";
-
-CREATE TYPE "public"."yes_no_enum" AS ENUM('Y', 'N', '-');
+CREATE TYPE "public"."yes_no_unknown_enum" AS ENUM('Y', 'N', '-');
 
 ALTER TABLE "public"."case"
 ALTER COLUMN medical_letter
-TYPE "public"."yes_no_enum" USING CASE
-  WHEN medical_letter::TEXT IN ('Y', 'N') THEN medical_letter::TEXT::"public"."yes_no_enum"
+TYPE "public"."yes_no_unknown_enum" USING CASE
+  WHEN medical_letter::TEXT IN ('Y', 'N') THEN medical_letter::TEXT::"public"."yes_no_unknown_enum"
   ELSE '-'
 END;
 
 ALTER TABLE "public"."case"
 ALTER COLUMN pa_report
-TYPE "public"."yes_no_enum" USING CASE
-  WHEN pa_report::TEXT IN ('Y', 'N') THEN pa_report::TEXT::"public"."yes_no_enum"
+TYPE "public"."yes_no_unknown_enum" USING CASE
+  WHEN pa_report::TEXT IN ('Y', 'N') THEN pa_report::TEXT::"public"."yes_no_unknown_enum"
   ELSE '-'
 END;
 
-DROP TYPE "public"."yes_no_enum__old_version_to_be_dropped";
+DROP TYPE "public"."yes_no_enum";
 
 ALTER TABLE "public"."case"
 ALTER COLUMN "medical_letter"
-SET DEFAULT '-'::yes_no_enum;
+SET DEFAULT '-'::yes_no_unknown_enum;
 
 ALTER TABLE "public"."case"
 ALTER COLUMN "medical_letter"
@@ -37,7 +34,7 @@ SET NOT NULL;
 
 ALTER TABLE "public"."case"
 ALTER COLUMN "pa_report"
-SET DEFAULT '-'::yes_no_enum;
+SET DEFAULT '-'::yes_no_unknown_enum;
 
 ALTER TABLE "public"."case"
 ALTER COLUMN "pa_report"
