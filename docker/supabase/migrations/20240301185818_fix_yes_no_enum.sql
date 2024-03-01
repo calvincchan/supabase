94e75e46609a0a88ebb6f736bbbcb0b963bc1beb
Calvin <calvin@edu.ink>
2024-03-01 11:12:16 -0800

    fix: type casing on migrating enum

--- docker/supabase/migrations/20240301185818_fix_yes_no_enum.sql
+++ docker/supabase/migrations/20240301185818_fix_yes_no_enum.sql
@@ -1,27 +1,44 @@
-alter table "public"."case" alter column "medical_letter" drop default;
+ALTER TABLE "public"."case"
+ALTER COLUMN "medical_letter"
+DROP DEFAULT;
 
-alter table "public"."case" alter column "pa_report" drop default;
+ALTER TABLE "public"."case"
+ALTER COLUMN "pa_report"
+DROP DEFAULT;
 
-alter type "public"."yes_no_enum" rename to "yes_no_enum__old_version_to_be_dropped";
+ALTER TYPE "public"."yes_no_enum"
+RENAME TO "yes_no_enum__old_version_to_be_dropped";
 
-create type "public"."yes_no_enum" as enum ('Y', 'N', '-');
+CREATE TYPE "public"."yes_no_enum" AS ENUM('Y', 'N', '-');
 
-alter table "public"."case" alter column medical_letter type "public"."yes_no_enum" using medical_letter::text::"public"."yes_no_enum";
+ALTER TABLE "public"."case"
+ALTER COLUMN medical_letter
+TYPE "public"."yes_no_enum" USING CASE
+  WHEN medical_letter::TEXT IN ('Y', 'N') THEN medical_letter::TEXT::"public"."yes_no_enum"
+  ELSE '-'
+END;
 
-alter table "public"."case" alter column pa_report type "public"."yes_no_enum" using pa_report::text::"public"."yes_no_enum";
+ALTER TABLE "public"."case"
+ALTER COLUMN pa_report
+TYPE "public"."yes_no_enum" USING CASE
+  WHEN pa_report::TEXT IN ('Y', 'N') THEN pa_report::TEXT::"public"."yes_no_enum"
+  ELSE '-'
+END;
 
-alter table "public"."case" alter column "medical_letter" set default None;
+DROP TYPE "public"."yes_no_enum__old_version_to_be_dropped";
 
-alter table "public"."case" alter column "pa_report" set default None;
+ALTER TABLE "public"."case"
+ALTER COLUMN "medical_letter"
+SET DEFAULT '-'::yes_no_enum;
 
-drop type "public"."yes_no_enum__old_version_to_be_dropped";
+ALTER TABLE "public"."case"
+ALTER COLUMN "medical_letter"
+SET NOT NULL;
 
-alter table "public"."case" alter column "medical_letter" set default '-'::yes_no_enum;
+ALTER TABLE "public"."case"
+ALTER COLUMN "pa_report"
+SET DEFAULT '-'::yes_no_enum;
 
-alter table "public"."case" alter column "medical_letter" set not null;
-
-alter table "public"."case" alter column "pa_report" set default '-'::yes_no_enum;
-
-alter table "public"."case" alter column "pa_report" set not null;
-
-
+ALTER TABLE "public"."case"
+ALTER COLUMN "pa_report"
+SET NOT NULL;
