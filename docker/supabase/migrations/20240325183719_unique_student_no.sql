7662a3cac9a905c88e4ad9d8445aa4f73c133a64
Calvin <calvin@edu.ink>
2024-03-25 15:13:14 -0700

    fix: not null default to empty string

--- docker/supabase/migrations/20240325183719_unique_student_no.sql
+++ docker/supabase/migrations/20240325183719_unique_student_no.sql
@@ -1,6 +1,6 @@
 ALTER TABLE "public"."case"
 ALTER COLUMN "student_no"
-SET NOT NULL;
+SET NOT NULL DEFAULT '';
 
 ALTER TABLE "public"."case"
 ADD CONSTRAINT "student_no_unique" UNIQUE ("student_no");
