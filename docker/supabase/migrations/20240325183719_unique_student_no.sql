138c59a1ebae823027030c3eafd2b02b0b364ac2
Calvin <calvin@edu.ink>
2024-03-25 15:16:08 -0700

    fix: split alter statements to two

--- docker/supabase/migrations/20240325183719_unique_student_no.sql
+++ docker/supabase/migrations/20240325183719_unique_student_no.sql
@@ -1,6 +1,10 @@
 ALTER TABLE "public"."case"
 ALTER COLUMN "student_no"
-SET NOT NULL DEFAULT '';
+SET DEFAULT '';
+
+ALTER TABLE "public"."case"
+ALTER COLUMN "student_no"
+SET NOT NULL;
 
 ALTER TABLE "public"."case"
 ADD CONSTRAINT "student_no_unique" UNIQUE ("student_no");
