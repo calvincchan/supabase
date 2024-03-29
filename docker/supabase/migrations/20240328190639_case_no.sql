7865faafaca46a217ec881f51908ce5c26e2e160
Calvin <calvin@edu.ink>
2024-03-28 22:36:50 -0700

    feat: editable case no

---
+++ docker/supabase/migrations/20240328190639_case_no.sql
@@ -0,0 +1,41 @@
+-- Add the new column
+ALTER TABLE "case"
+ADD COLUMN "case_no" TEXT;
+
+-- Add the new column to the view
+CREATE OR REPLACE VIEW
+  "public"."my_case" AS
+SELECT
+  a.id,
+  a.created_at,
+  a.student_name,
+  a.student_no,
+  a.updated_at,
+  a.updated_by,
+  a.is_archived,
+  a.archived_at,
+  a.archived_by,
+  a.case_status,
+  a.updated_by_name,
+  a.grade,
+  a.homeroom,
+  a.created_by,
+  a.created_by_name,
+  a.tier,
+  a.last_session_at,
+  a.next_session_at,
+  a.last_session_by,
+  a.last_session_by_name,
+  a.handlers,
+  a.student_first_name,
+  a.student_last_name,
+  a.background,
+  a.student_other_name,
+  a.case_no
+FROM
+  (
+    "case" a
+    JOIN case_handler b ON ((a.id = b.case_id))
+  )
+WHERE
+  (b.user_id = auth.uid ());
---
+++ docker/supabase/migrations/20240328215508_case_default_value.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "public"."session"
+ALTER COLUMN "language"
+SET DEFAULT 'E'::bpchar;
