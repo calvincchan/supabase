34c103c57ceeb81bcef8b18bd1ac1607963eb388
Calvin <calvin@edu.ink>
2024-04-14 22:18:03 -0700

    fix: contact to create case handler

---
+++ docker/supabase/migrations/20240415051534_fix_contact_create_case.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+  role_permission (ROLE, permission)
+VALUES
+  ('Li Ren Contact', 'case_handler:create'),
+  ('Li Ren Contact', 'case_handler:edit'),
+  ('Li Ren Contact', 'case_handler:delete');
