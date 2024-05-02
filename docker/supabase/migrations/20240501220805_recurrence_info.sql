105d2ded52a4693c94b378805c883181bec310b3
Calvin <calvin@edu.ink>
2024-05-02 14:35:56 -0700

    fix: session recurrence

---
+++ docker/supabase/migrations/20240501220805_recurrence_info.sql
@@ -0,0 +1,31 @@
+-- store a copy of the start date of the recurrence
+ALTER TABLE "public"."session"
+ADD COLUMN "recurrence_start_date" TIMESTAMP WITH TIME ZONE;
+
+-- drop column "time_frame_until"
+ALTER TABLE "public"."session"
+DROP COLUMN "time_frame_until";
+
+-- fix the recurrence parent with null value
+UPDATE "public"."session"
+SET
+  recurrence_parent = id
+WHERE
+  recurrence_parent IS NULL;
+
+-- update the recurrence start date with the start date of the recurrence parent
+UPDATE "public"."session" a
+SET
+  recurrence_start_date = (
+    SELECT
+      start_date
+    FROM
+      "public"."session"
+    WHERE
+      id = a.recurrence_parent
+    LIMIT
+      1
+  )
+WHERE
+  time_frame = 'B'
+  AND recurrence_parent IS NOT NULL;
