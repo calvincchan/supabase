c94fafef19ba0fe0c65676a296e57b9530eac697
Calvin <calvin@edu.ink>
2024-05-03 18:12:26 -0700

    fix: time extension multiple values

---
+++ docker/supabase/migrations/20240503204008_time_extension_array.sql
@@ -0,0 +1,20 @@
+-- convert column "iaa_time_extension" from iaa_time_extension_enum to iaa_time_extension_enum[]
+-- convert NULL to empty array
+ALTER TABLE "public"."case"
+ALTER COLUMN iaa_time_extension
+SET DATA TYPE iaa_time_extension_enum[]
+USING ARRAY[iaa_time_extension]::iaa_time_extension_enum[];
+
+UPDATE "public"."case"
+SET
+  iaa_time_extension = ARRAY[]::iaa_time_extension_enum[]
+WHERE
+  iaa_time_extension = ARRAY[NULL]::iaa_time_extension_enum[];
+
+ALTER TABLE "public"."case"
+ALTER COLUMN iaa_time_extension
+SET DEFAULT {}::iaa_time_extension_enum[];
+
+ALTER TABLE "public"."case"
+ALTER COLUMN iaa_time_extension
+SET NOT NULL;
