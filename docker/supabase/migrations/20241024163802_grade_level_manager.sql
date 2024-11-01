ddf6ad74add80a0ad65dadbff54c2fa0ef2329ab
Calvin C Chan <hello@calvin-c.com>
2024-11-01 15:47:54 -0700

    fix: revert the migration to remove grade level manager and use gll for the same purpose

--- docker/supabase/migrations/20241024163802_grade_level_manager.sql
+++
@@ -1,3 +0,0 @@
--- add a new role called 'Grade Level Manager'
-ALTER TYPE role_enum
-ADD VALUE 'Grade Level Manager';
--- docker/supabase/migrations/20241031171711_gll_managed_grades.sql
+++ docker/supabase/migrations/20241031171711_gll_managed_grades.sql
@@ -2,25 +2,7 @@
 ALTER TABLE "public"."team_member"
 ADD COLUMN "managed_grades" grade_enum[] NOT NULL DEFAULT '{}'::grade_enum[];
 
--- add permissions for the new role
-INSERT INTO
-  "public"."role_permission" ("role", "permission")
-VALUES
-  ('Grade Level Manager', 'dashboard:list'),
-  ('Grade Level Manager', 'case:list'),
-  ('Grade Level Manager', 'case:edit'),
-  ('Grade Level Manager', 'my_case:list'),
-  ('Grade Level Manager', 'progress_note:list'),
-  ('Grade Level Manager', 'progress_note:create'),
-  ('Grade Level Manager', 'reminder:list'),
-  ('Grade Level Manager', 'reminder:create'),
-  ('Grade Level Manager', 'session:list'),
-  ('Grade Level Manager', 'session:create'),
-  ('Grade Level Manager', 'target:list'),
-  ('Grade Level Manager', 'target:create'),
-  ('Grade Level Manager', 'remark:list');
-
--- function to get managed_grades (grade_enum[]) from team_member table if the current user is a Grade Level Manager
+-- function to get managed_grades (grade_enum[]) from team_member table
 CREATE
 OR REPLACE FUNCTION public.get_managed_grades () RETURNS grade_enum[] LANGUAGE plpgsql AS $function$
 DECLARE
@@ -36,7 +18,7 @@ $function$;
 CREATE
 OR REPLACE FUNCTION public.should_apply_grade_filter () RETURNS BOOLEAN LANGUAGE plpgsql AS $function$
 BEGIN
-  RETURN (auth.jwt() ->> 'user_role') = 'Grade Level Manager';
+  RETURN (auth.jwt() ->> 'user_role') = 'GLL';
 END;
 $function$;
 
