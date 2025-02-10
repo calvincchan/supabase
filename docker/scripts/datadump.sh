6b0e9d0cc624f63ef77a4709fbafa244729ca1b8
Calvin C Chan <hello@calvin-c.com>
2025-02-09 21:45:10 -0800

    feat: replace fulldump script with datadump script and update .gitignore for data.sql

--- docker/scripts/datadump.sh
+++ docker/scripts/datadump.sh
@@ -2,4 +2,4 @@ source .env
 source .env.script
 DB_URL=postgresql://postgres:$POSTGRES_PASSWORD@127.0.0.1:$POSTGRES_PORT/$POSTGRES_DB
 
-supabase db dump --schema $POSTGRES_DUMP_SCHEMAS --db-url "$DB_URL" > ./supabase/dumps/fulldump.sql
+supabase db dump --schema $POSTGRES_DUMP_SCHEMAS --db-url "$DB_URL" --data-only > ./supabase/dumps/data.sql
