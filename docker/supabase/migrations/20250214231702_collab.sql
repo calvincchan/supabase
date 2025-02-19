bf3c8e845848038f689b6ba924d2e5fbeae38bac
Calvin C Chan <hello@calvin-c.com>
2025-02-19 12:54:22 -0800

    fix: remove delete from session trigger that calls the process_session function

--- docker/supabase/migrations/20250214231702_collab.sql
+++ docker/supabase/migrations/20250214231702_collab.sql
@@ -1,10 +1,6 @@
 --
 -- Drop all triggers and functions for session and merge them into one trigger and function
 --
-DROP TRIGGER IF EXISTS "trigger_set_next_upcoming_session" ON "public"."session";
-
-DROP FUNCTION IF EXISTS "trigger_set_next_upcoming_session";
-
 DROP TRIGGER IF EXISTS "session_i" ON "public"."session";
 
 DROP FUNCTION IF EXISTS "public"."insert_session_collaborator";
@@ -13,8 +9,9 @@ DROP TRIGGER IF EXISTS "trigger_on_session_status" ON "public"."session";
 
 DROP FUNCTION IF EXISTS "public"."trigger_on_session_status";
 
----
----
+--
+-- Handle session insert and update - creator, timestamps, and collaborators
+--
 CREATE
 OR REPLACE FUNCTION public.process_session () RETURNS TRIGGER LANGUAGE plpgsql AS $function$
 DECLARE
@@ -29,9 +26,9 @@ BEGIN
     NEW.collaborator_users := ARRAY[auth.uid()];
   END IF;
 
-  --- On update:
-  --- 1. set session started_at, started_by, started_by_name
-  --- 2. set case last_session_at, last_session_by, last_session_by_name
+  -- On update:
+  -- 1. set session started_at, started_by, started_by_name
+  -- 2. set case last_session_at, last_session_by, last_session_by_name
   IF TG_OP = 'UPDATE' THEN
     IF OLD.status = 'U' AND (NEW.status = 'I' OR NEW.status = 'X') THEN
       UPDATE "case" SET last_session_at = NOW(), last_session_by = auth.uid(), last_session_by_name = get_name() WHERE id = NEW.case_id;
@@ -47,16 +44,14 @@ BEGIN
     END IF;
   END IF;
 
-  -- On insert and update: set session.collaborators
-  If TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
-    p_users := NEW.collaborator_users;
-    --- find the name of all users from team_member table where id is in p_users array
-    SELECT STRING_AGG(name, '|' ORDER BY name ASC) INTO p_collaborators
-    FROM team_member
-    WHERE id = ANY(p_users);
-    --- Set session.collaborators with the new value of p_collaborators
-    NEW.collaborators := COALESCE(p_collaborators, '');
-  END IF;
+  -- cache collaborator names to session.collaborators
+  p_users := NEW.collaborator_users;
+  -- find the name of all users from team_member table where id is in p_users array
+  SELECT STRING_AGG(name, '|' ORDER BY name ASC) INTO p_collaborators
+  FROM team_member
+  WHERE id = ANY(p_users);
+  -- Set session.collaborators with the new value of p_collaborators
+  NEW.collaborators := COALESCE(p_collaborators, '');
 
   RETURN NEW;
 END;
@@ -64,8 +59,7 @@ $function$;
 
 ALTER FUNCTION public.process_session () OWNER TO postgres;
 
-CREATE TRIGGER session_i_u_d BEFORE INSERT
+CREATE TRIGGER session_i_u BEFORE INSERT
 OR
-UPDATE
-OR DELETE ON public.session FOR EACH ROW
+UPDATE ON public.session FOR EACH ROW
 EXECUTE FUNCTION process_session ();
